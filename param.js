28972972c76283b8b6e6e6c3b0c4d6c058d47248
YanCallier <45624004+YanCallier@users.noreply.github.com>
2019-02-06 11:55:17 +0100

    Add files via upload

---
+++ param.js
@@ -0,0 +1,53 @@
+﻿//*** Ce fichier a été créé essentiellement pour pouvoir ajouter simplement des plateformes au jeux, et des briques de plateformes. Nous y avons ajouté petit à petit différentes variables du jeu, permettant de faire des tests de rendus graphiques et de jouabilité.
+
+var param = {
+
+    illustrationHalo:"halo.png",
+    illustrationflashMonster:"flash_monster.png",
+
+    perso: {
+        nom:"Roger",
+        spriteCourse: "bande_perso.png",
+        nbImageCourse: 5,
+        nbImageSaute:4,
+        impulsion:10,
+        poid: 0.1,
+        spriteSpeed: 4,
+        inertie: 4
+    },
+    plateforme: {
+        hauteur:500,
+        vitesse: 2,
+        acceleration: 0.001, // attention ça va très vite.
+        espacementMin: 0,
+        espacementMax: 0,
+
+        //*** On peut ajouter/enlever autant de plateformes et de briques centrales de plateforme qu'on veut, sans toucher le reste du code
+        sources: [
+                {
+                    ligneDeFlottaison : 6, //* definit la ligne sur laquelle le perso évolue
+                    debut:"plateforme_bord.png",
+                    centre:["plateforme_centre.png","plateforme_centre.png","plateforme_centre.png","plateforme_centre2.png"],
+                    fin:"plateforme_bord2.png",
+                },
+                {
+                    ligneDeFlottaison : 6,
+                    debut:"plateforme_bord.png",
+                    centre:["plateforme_centre.png","plateforme_centre2.png","ralentisseur.png"],
+                    fin:"plateforme_bord2.png",
+                },
+                {
+                    ligneDeFlottaison : 6,
+                    debut:"plateforme_bord.png",
+                    centre:["plateforme_centre.png","plateforme_centre2.png"],
+                    fin:"plateforme_bord2.png",
+                },
+                {
+                    ligneDeFlottaison : 70,
+                    debut:"rocher_volant.png",
+                    centre:["vide.gif"],
+                    fin:"vide.gif",
+                }
+            ]
+    },
+};
